dd1defdfb4e4216478f1519cbde3ec72118a2ecd
komarikl <ladyxa@yandex.ru>
2015-07-01 18:30:46 +0300

    Добавлен скрипт totop

---
+++ dist/scripts/totop.js
@@ -0,0 +1,13 @@
+$(function() {
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('#to-top').fadeIn();
+		} else {
+			$('#to-top').fadeOut();
+				}
+	});
+$('#to-top').click(function() {
+	$('body,html').animate({scrollTop:0},800); 
+});
+
+});
